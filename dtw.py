492285ef2c7514e946bd86509392f120380c1039
sarmstr5 <git.sarmstr5@gmail.com>
2017-03-07 08:16:27 -0500

    working on fixing dtw

--- dtw.py
+++ dtw.py
@@ -35,10 +35,10 @@ def find_initial_j(i, dtw_width):
 def create_dist_mx(verbose, x, y, dtw_width):
     # create an array
     assert x.shape == y.shape, 'The shapes dont match'
+    # create an extra column and row for path traverseral
+    # makes it so I do not have to make conditional checks for out of index errors
     dist_mx = np.zeros(shape=(x.shape[0], y.shape[0]))
     dist_mx[:] = np.inf
-    i = 0
-    j = 0
     for i in range(0, dist_mx.shape[0]):
         # save time by starting within the dtw width
         initial_j = find_initial_j(i, dtw_width)
@@ -46,19 +46,30 @@ def create_dist_mx(verbose, x, y, dtw_width):
             if (j - i) > dtw_width:
                 break
             dist_mx[i][j] = np.abs(x[i] - y[j])
+    path_mx = np.insert(dist_mx,0,np.inf,axis=0)
+    path_mx = np.insert(path_mx,0,np.inf,axis=1)
+    path_mx[0,0] = 0
     print(dist_mx)
-    for i in range(0, dist_mx.shape[0]):
+    for i in range(0, path_mx.shape[0]-1):
         # save time by starting within the dtw width
         initial_j = find_initial_j(i, dtw_width)
-        for j in range(initial_j, dist_mx.shape[1]-1):
+        for j in range(initial_j, path_mx.shape[1]-1):
             if (j - i) > dtw_width:
                 break
+            if path_mx[i, j] == np.inf:
+                path_mx[i,j] = 0
+            # elif (i==0) and (j==0):
+            #     continue
             # [index, value] i, j
-            print('i:{} j:{}'.format(i,j))
-            step, step_i , step_j = get_next_step(i,j,dist_mx)
-            dist_mx[step_i][step_j] += step[1]
-    print(dist_mx)
-    return dist_mx
+            # path_mx[i, j] += get_next_step(i, j, dist_mx)
+            step = [path_mx[i+1][j+1], path_mx[i][j+1], path_mx[i+1][j]]
+            path_mx[i, j] += min(step)
+            print('i:{}\tj:{}\tvalue:{}'.format(i,j,path_mx[i,j]))
+            print(step)
+            print(path_mx)
+            # dist_mx[step_i][step_j] += step[1]
+    print(path_mx)
+    return path_mx
 
 def shortest_path(verbose, dist_arr, width=None):
     # should i start at the end or begining
@@ -72,7 +83,7 @@ def shortest_path(verbose, dist_arr, width=None):
     path_cost = [] # keep cost of each step
     while (i != last_i) or (j != last_j):
         # [index, value], i, j.  matrix will be in memory now 3 times? or am i just passing by reference
-        path_arr, step, i, j = get_next_step(i, j, path_arr)
+        step, i, j = get_next_step(i, j, path_arr)
 
         path.append([i, j])
         path_cost.append(step[1])
@@ -95,17 +106,17 @@ def get_next_step(i, j, dist_mx):
     next_step = [np.argmin(step_mx), min(step_mx)]  # [index, value]
 
     # which step was taken
-    if next_step[0] == 0:  # diagonal step
-        dist_mx[i+1][j+1] += dist_mx[i][j]
-        i += 1
-        j += 1
-    elif next_step[0] == 1:  # column increases, step right
-        dist_mx[i][j+1] += dist_mx[i][j]
-        j += 1
-    else: # step down
-        dist_mx[i+1][j] += dist_mx[i][j]
-        i += 1
-    return next_step, i, j
+    # if next_step[0] == 0:  # diagonal step
+    #     dist_mx[i+1][j+1] += dist_mx[i][j]
+    #     i += 1
+    #     j += 1
+    # elif next_step[0] == 1:  # column increases, step right
+    #     dist_mx[i][j+1] += dist_mx[i][j]
+    #     j += 1
+    # else: # step down
+    #     dist_mx[i+1][j] += dist_mx[i][j]
+    #     i += 1
+    return next_step[1]
 
 def main():
     verbose = True
